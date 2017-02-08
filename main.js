c818c0d38b8ceb99aaf8b867407b4d0397c6ef12
brandonassing <bassing@uwo.ca>
2017-02-07 20:21:02 -0500

    new dev branch

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-$(document).ready(function(){
-  var backgrounds = new Array(
-    'url(./img/background.jpg)',
-    'url(./img/background2.jpg)'
-  );
-
-  var t = 0;
-setInterval(function () {
-      $('body').css("background-image",backgrounds[t]);
-      t += 1;
-      if(t>backgrounds.length)
-      {
-        t=0;
-      }
-  },4000);
+$(document).ready(function() {
+    var backgrounds = new Array(
+        'url(./img/background.jpg)',
+        'url(./img/background2.jpg)'
+    );
+    var t = 0;
+    setInterval(function() {
+        $('body').css("background-image", backgrounds[t]);
+        t += 1;
+        if (t > backgrounds.length) {
+            t = 0;
+        }
+    }, 4000);
 
 });

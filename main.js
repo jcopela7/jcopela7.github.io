aed517a09d134c416995cc4cea1390b7232cb773
jcopela7 <joncope8@gmail.com>
2017-02-07 20:13:14 -0500

    Fade Background and Nav Bar

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  var backgrounds = new Array(
+    'url(./img/background.jpg)',
+    'url(./img/background2.jpg)'
+  );
+
+  var t = 0;
+setInterval(function () {
+      $('body').css("background-image",backgrounds[t]);
+      t += 1;
+      if(t>backgrounds.length)
+      {
+        t=0;
+      }
+  },4000);
+
+});
